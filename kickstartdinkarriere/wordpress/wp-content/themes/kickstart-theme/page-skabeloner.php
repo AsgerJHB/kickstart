0c5c3d4334e1b1366d5e14b4c16525f54d558d7b
saschahaarup <sasc0678@stud.kea.dk>
2022-05-30 18:44:05 +0200

    json

--- kickstartdinkarriere/wordpress/wp-content/themes/kickstart-theme/page-skabeloner.php
+++ kickstartdinkarriere/wordpress/wp-content/themes/kickstart-theme/page-skabeloner.php
@@ -97,7 +97,7 @@ while ( have_posts() ) :
           if (filter == "alle" || filter == skabelon.simpel || filter == skabelon.farverig || filter == skabelon.formel || filter == skabelon.kreativ) {
             //hvis objektet har samme værdi som filterknappen
             const clone = template.cloneNode(true);
-            clone.querySelector(".billede").`url(${skabelon.billede.guid})`;
+            clone.querySelector(".billede").src=`${skabelon.billede.guid}`;
             clone.querySelector(".overskrift").textContent = `${skabelon.title.rendered}`;
             clone.querySelector(".kort_beskrivelse").textContent = `${skabelon.kortbeskrivelse}`;
             clone.querySelector(".pris").textContent = `${skabelon.pris}`;
