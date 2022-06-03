c5839b0435f1e7ac9dce4ca8c983e174c15c06a2
Asger <asge0729@stud.kea.dk>
2022-06-03 12:14:24 +0200

    uhbushbf

--- kickstartdinkarriere/wordpress/wp-content/themes/kickstart-theme/single-skabelon.php
+++ kickstartdinkarriere/wordpress/wp-content/themes/kickstart-theme/single-skabelon.php
@@ -128,7 +128,7 @@ get_header(); ?>
 				document.querySelector(".lang_beskrivelse").innerHTML = `${skabelon.lang_beskrivelse}`;
           		document.querySelector(".billede").src = `${skabelon.billede.guid}`;
 				document.querySelector(".pris").textContent = `${skabelon.pris + " kr."}`; 
-        document.querySelector(".koeb").src("click", () => location.href = `${skabelon._links}`); 
+        document.querySelector(".koeb").src("click", () => location.href = `${skabelon.linkweb}`); 
             mainContent.appendChild(clone);
 			  }
 
